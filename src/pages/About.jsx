0952f1f4c82db2b4b3d3a9585801f8a2b44a4a6c
Abhisth Chatterji <chatterjiabhisth@gmail.com>
2024-01-22 23:25:53 +0530

    changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -15,9 +15,8 @@ const About = () => {
         Hello, I'm{" "}
         <span className='blue-gradient_text font-semibold drop-shadow'>
           {" "}
-          Abhisth
+          Abhisth Chatterji
         </span>{" "}
-        👋
       </h1>
 
       <div className='mt-5 flex flex-col gap-3 text-slate-500'>
@@ -25,23 +24,15 @@ const About = () => {
         Hello there! I'm Abhisth Chatterji, a final year BTech Computer Science Engineering, with a passion for exploring the ever-evolving world of technology. I believe in the power of continuous learning and adaptability in the fast-paced field of computer science. 
         </p>
         <p>
-        🌐 Cloud Computing & DevOps: Proficient in cloud platforms such as AWS and Azure, I have experience in building CI/CD pipelines, Infrastructure as Code (IaC), containerization with Docker, and orchestrating applications with Kubernetes. My expertise also extends to configuration management using Ansible, version control with Git, and continuous integration with Jenkins.
-        </p>
+        🌐 Cloud Computing & DevOps: Proficient in cloud platforms such as AWS and Azure.</p>
         <p>
-        📈 Testing, Monitoring & Logging: I understand the importance of ensuring software quality. I have knowledge of testing methodologies and tools, as well as setting up monitoring and logging systems to maintain the health and performance of applications.
-        </p>
+        📈 Testing, Monitoring & Logging: I understand the importance of ensuring software quality.</p>
         <p>
-        📊 Data Analysis and Engineering: I enjoy working with data to derive insights and make informed decisions. Proficient in Microsoft Excel, Power BI, and Tableau, I can transform raw data into meaningful visualizations. I'm also skilled in database management, utilizing tools like MySQL, Pandas, and Scikit-learn for data manipulation and analysis.
-        </p>
+        📊 Data Analysis and Engineering: I enjoy working with data to derive insights and make informed decisions.</p>
         <p>
-        🧠 AI/ML (Basics): I have a fundamental understanding of Artificial Intelligence and Machine Learning concepts, which I hope to further develop as I progress in my career. I'm excited about the potential of AI/ML in solving real-world problems.
-        </p>
+        🧠 AI/ML (Basics): I have a fundamental understanding of Artificial Intelligence and Machine Learning concepts.</p>
         <p>
-        💻 Programming Languages: I'm well-versed in Java and Python, which I use for various programming tasks, including web development as a Full-Stack developer. I have experience in Object-Oriented Programming and am comfortable with Data Structures and Algorithms.
-        </p>
-        <p>
-        🖥️ Operating System & Networking: While my knowledge in this area is in its early stages, I have a basic understanding of operating systems and networking principles, which I plan to expand upon.
-        </p>
+        💻 Programming Languages: I'm well-versed in Java and Python.</p>
       </div>
 
       {/* <div className='py-10 flex flex-col'>
